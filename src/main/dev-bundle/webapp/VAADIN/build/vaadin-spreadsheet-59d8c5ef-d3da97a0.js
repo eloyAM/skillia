e88aa61eb3c499d34ae56b7cb851baaad1cc08c9
Eloy AM <eloy.abellanm@um.es>
2023-11-17 10:30:45 +0100

    dev-bundle gen files (to avoid recompile in dev mode, not valid for prod)

---
+++ src/main/dev-bundle/webapp/VAADIN/build/vaadin-spreadsheet-59d8c5ef-d3da97a0.js
@@ -0,0 +1,1 @@
+import{I as e}from"./indexhtml-3cd5dc06.js";const r={tagName:"vaadin-spreadsheet",displayName:"Spreadsheet",elements:[{selector:"vaadin-spreadsheet",displayName:"Spreadsheet",properties:[e.backgroundColor,e.borderColor,e.borderWidth,e.borderRadius,e.padding]}]};export{r as default};
---
+++ src/main/dev-bundle/webapp/VAADIN/build/vaadin-tabsheet-dd99ed9a-1be41943.js
@@ -0,0 +1,1 @@
+import{I as e}from"./indexhtml-3cd5dc06.js";const r={tagName:"vaadin-tabsheet",displayName:"Tab Sheet",elements:[{selector:"vaadin-tabsheet",displayName:"TabSheet",properties:[e.padding,e.backgroundColor,e.borderWidth,e.borderColor,e.borderRadius]}]};export{r as default};
