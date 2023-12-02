0dfbd0cadd02b69125c318515bde5fbab2ac6b0f
Eloy AM <eloy.abellanm@um.es>
2023-12-02 22:12:50 +0100

    Base rest controllers path "/api"

---
+++ src/main/java/com/example/application/config/RestConfig.java
@@ -0,0 +1,17 @@
+package com.example.application.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class RestConfig implements WebMvcConfigurer {
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api", HandlerTypePredicate.forAnnotation(RestController.class));
+    }
+}
