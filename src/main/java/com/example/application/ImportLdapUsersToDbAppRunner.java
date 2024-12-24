64cb82d437e91ccbd7d86ddfc22061fff3971d55
eloyAM <eloy.abellanm@um.es>
2024-12-24 19:59:27 +0100

    Fix embedded LDAP

--- src/main/java/com/example/application/ImportLdapUsersToDbAppRunner.java
+++ src/main/java/com/example/application/ImportLdapUsersToDbAppRunner.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile({"!embedded-ldap"})
 public class ImportLdapUsersToDbAppRunner implements ApplicationRunner {
 
     private final LdapDbService ldapDbService;
