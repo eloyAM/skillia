33f4399ec1bb0044fa220cafb0466731c36a34f3
eloyAM <eloy.abellanm@um.es>
2024-12-25 17:53:35 +0100

    Review profile properties and testing

--- src/test/java/com/example/application/external/DbInitFromLdapTest.java
+++ src/test/java/com/example/application/external/DbInitFromLdapTest.java
@@ -1,4 +1,4 @@
-package com.example.application.it;
+package com.example.application.external;
 
 import com.example.application.dto.PersonDto;
 import com.example.application.service.PersonService;
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
@@ -18,10 +19,16 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles("h2")   // Be careful to don't runt it with the embedded postgres or LDAP to avoid conflicts
 @SpringBootTest
 @Testcontainers
 public class DbInitFromLdapTest {
     private static final int LDAP_PORT = 1389;
+    private static final String LDAP_BASE = "dc=example,dc=org";
+    public static final String LDAP_ADMIN_SIMPLE_USERNAME = "admin-testcontainers";
+    private static final String LDAP_ADMIN_DN = "cn=" + LDAP_ADMIN_SIMPLE_USERNAME + ",dc=example,dc=org";
+    private static final String LDAP_ADMIN_PWD = "adminpassword";
+
     @Autowired
     private PersonService personService;
     @Container
@@ -31,14 +38,17 @@ public class DbInitFromLdapTest {
     private static final GenericContainer<?> openLdapContainer = new GenericContainer<>("bitnami/openldap:2.6.6-debian-11-r59")
             .withExposedPorts(LDAP_PORT)
             .withCopyFileToContainer(MountableFile.forHostPath("./src/test/resources/ldif/test-01.ldif"), "/ldifs/test-01.ldif")
-            .withEnv("LDAP_ADMIN_USERNAME", "admin")
-            .withEnv("LDAP_ADMIN_PASSWORD", "adminpassword")
-            .withEnv("LDAP_ROOT", "dc=example,dc=org")
-            .withEnv("LDAP_ADMIN_DN", "cn=admin,dc=example,dc=org");
+            .withEnv("LDAP_ADMIN_USERNAME", LDAP_ADMIN_SIMPLE_USERNAME)
+            .withEnv("LDAP_ADMIN_PASSWORD", LDAP_ADMIN_PWD)
+            .withEnv("LDAP_ROOT", LDAP_BASE)
+            .withEnv("LDAP_ADMIN_DN", LDAP_ADMIN_DN);
 
     @DynamicPropertySource
     private static void initProperties(DynamicPropertyRegistry registry) {
-        registry.add("ldap.url", () -> "ldap://localhost:" + openLdapContainer.getMappedPort(LDAP_PORT));
+        registry.add("spring.ldap.urls", () -> "ldap://localhost:" + openLdapContainer.getMappedPort(LDAP_PORT));
+        registry.add("spring.ldap.base", () -> LDAP_BASE);
+        registry.add("spring.ldap.username", () -> LDAP_ADMIN_DN);
+        registry.add("spring.ldap.password", () -> LDAP_ADMIN_PWD);
     }
 
     @Test
